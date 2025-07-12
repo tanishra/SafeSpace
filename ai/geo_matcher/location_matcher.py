60c21c590a068f85fac6945a641e8307513eb193
Tanish Rajput <tanishrajput9@gmail.com>
2025-07-12 13:34:36 +0530

    updated location_matcher.py

--- ai/geo_matcher/location_matcher.py
+++ ai/geo_matcher/location_matcher.py
@@ -1,31 +1,9 @@
 from geopy.distance import geodesic
-from transformers import pipeline
-import torch 
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'threat_detection')))
+from threat_detection import detect_threat
 
-device = torch.device('cpu')
-
-local_model_path = "/Users/tanishrajput/Desktop/SafeSpace/ai/nlp_engine/bhadresh-bert-emotion-model"
-classifier = pipeline("text-classification", model=local_model_path, tokenizer=local_model_path)
-
-def detect_threat(text):
-    result = classifier(text)[0]
-    return result["label"], result["score"]
-
-
-def classify_threat(text):
-    """
-    Function to classify a given threat text into emotional categories like 'anger', 'fear', etc.
-    """
-    label, score = detect_threat(text)
-    if label == 'ANGER': 
-        return 'High threat', score
-    elif label == 'FEAR':
-        return 'Medium threat', score
-    elif label == 'SADNESS':
-        return 'Low threat', score
-    else:
-        return 'Low threat', score
-    
 
 def is_threat_near_user(user_coords, threat_coords, threat_text, max_km=10):
     """
@@ -36,23 +14,17 @@ def is_threat_near_user(user_coords, threat_coords, threat_text, max_km=10):
     distance = geodesic(user_coords, threat_coords).km
 
     # Classify the threat based on the text
-    threat_class, confidence = classify_threat(threat_text)
+    result = detect_threat([threat_text])[0]
+    threat_class = result['threat_level']
+    confidence = result['score_percent']
 
-    # Determine if the threat is near the user
-    if distance <= max_km:
-        return True, threat_class, confidence  # Threat is near, with classification and confidence
-    else:
-        return False, threat_class, confidence
+    if distance > max_km:
+        return False, confidence, threat_class,"Threat is too far away - {:.2f} km".format(distance) 
     
+    if threat_class == "No Threat":
+        return False, confidence, threat_class, distance
 
-user_coords = (40.7128, -74.0060)  
-threat_coords = (40.7306, -73.9352)  
-threat_text = "I'm going to destroy everything."  
-max_km = 10  
-
-# Call the function
-is_near, threat_type, confidence = is_threat_near_user(user_coords, threat_coords, threat_text, max_km)
-
-# Output the result
-print(f"Is the threat near? {is_near}")
-print(f"Threat classification: {threat_type} (Confidence: {confidence:.2f})")
+    # Threat is valid and within range
+    distance = round(distance, 2)  
+    return True, confidence, threat_class, distance
+    

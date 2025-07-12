5c9da6c5cc62517f83f52f5ed02fd9fad737661b
Tanish Rajput <tanishrajput9@gmail.com>
2025-07-12 17:46:50 +0530

    updated model.py

--- ai/nlp_engine/model.py
+++ ai/nlp_engine/model.py
@@ -1,13 +1,13 @@
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from transformers import pipeline, BertForTokenClassification, BertTokenizer
 
-# Specify the model name on Hugging Face Model Hub
-model_name = "bhadresh-savani/bert-base-uncased-emotion"
-local_model_path = "bhadresh-bert-emotion-model"  # Directory to save model files
+# Download and save the model and tokenizer
+model_name = "dbmdz/bert-large-cased-finetuned-conll03-english"
 
-# Download the model and tokenizer from Hugging Face
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+# Load model and tokenizer from Hugging Face
+model = BertForTokenClassification.from_pretrained(model_name)
+tokenizer = BertTokenizer.from_pretrained(model_name)
 
 # Save model and tokenizer locally
-model.save_pretrained(local_model_path)
-tokenizer.save_pretrained(local_model_path)
+model.save_pretrained("./bert-large-ner")
+tokenizer.save_pretrained("./bert-large-ner")
+
